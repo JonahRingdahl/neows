1cd2d9ed0a1a303d2b02ae345d1d246d58a99c8d
Jonah Ringdahl <JonahRingdahl@outlook.com>
2026-01-17 15:41:02 -0500

    change to data struct

--- src/Neo/Diameter.hpp
+++ src/Neo/Diameter.hpp
@@ -2,8 +2,7 @@
 #include <nlohmann/json.hpp>
 #include <tuple>
 
-class Diameter {
-public:
+struct Diameter {
   Diameter(nlohmann::json diameter_json);
   Diameter(Diameter &&) = default;
   Diameter(const Diameter &) = default;
@@ -11,27 +10,8 @@ public:
   Diameter &operator=(const Diameter &) = default;
   ~Diameter();
 
-  std::tuple<double, double> GetKilo() { return kilometer_min_max; }
-  std::tuple<double, double> GetMeter() { return meter_min_max; }
-  std::tuple<double, double> GetMiles() { return miles_min_max; }
-  std::tuple<double, double> GetFeet() { return feet_min_max; }
-
-  void SetKilo(double min, double max) {
-    this->kilometer_min_max = std::make_tuple(min, max);
-  }
-  void SetMeter(double min, double max) {
-    this->meter_min_max = std::make_tuple(min, max);
-  }
-  void SetMiles(double min, double max) {
-    this->miles_min_max = std::make_tuple(min, max);
-  }
-  void SetFeet(double min, double max) {
-    this->feet_min_max = std::make_tuple(min, max);
-  }
-
   void DisplayDiameter();
 
-private:
   std::tuple<double, double> kilometer_min_max;
   std::tuple<double, double> meter_min_max;
   std::tuple<double, double> miles_min_max;
